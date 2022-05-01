9ac90bc9ce96a82761cc9f6bf16e64c677703720
LV <whgodbs32191@gmail.com>
2022-05-01 22:50:58 +0900

    아직 ajax 안됨

--- src/main/java/test/running/weather/service/WeatherPlaceService.java
+++ src/main/java/test/running/weather/service/WeatherPlaceService.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
+
+import com.google.gson.Gson;
 
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -12,13 +15,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import lombok.RequiredArgsConstructor;
+import test.running.weather.domain.WeatherDataRepository;
+import test.running.weather.web.dto.dataCraw.PlaceDto;
+import test.running.weather.web.dto.dataCraw.RootDto;
 
 @RequiredArgsConstructor
 @Service
 @Component
 public class WeatherPlaceService {
-    public String rowNumDown(String courseId) {
+    private final WeatherDataRepository weatherDataRepository;
+
+    public List<PlaceDto> placeList(String courseId) {
         StringBuilder sb = new StringBuilder();
+        Gson gson = new Gson();
         try {
             String uri = "http://apis.data.go.kr/1360000/TourStnInfoService/getTourStnVilageFcst?serviceKey=406lrJVRiqLfSp8HVHaLUY%2Bo3a%2F1DtuHUz%2Fs9waJGTnvPuodbdMnS9eCCmMQFvRJUakGRGcnSaYJtm27C8IcjQ%3D%3D&pageNo=1&numOfRows=10&dataType=JSON&CURRENT_DATE=2019122010&HOUR=24&COURSE_ID="
                     + courseId;
@@ -31,16 +40,17 @@ public class WeatherPlaceService {
             while ((returnLine = br.readLine()) != null) {
                 sb.append(returnLine);
             }
+            RootDto dto = gson.fromJson(sb.toString(), RootDto.class);
 
-            urlConnection.disconnect();
+            List<PlaceDto> rList = dto.getResponse().getBody().getItems().getItem();
 
-            System.out.println(sb.toString());
-            return sb.toString();
+            // System.out.println(sb.toString());
+            return rList;
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return "page/main";
+        return null;
 
     }
 
--- src/main/java/test/running/weather/web/WeatherController.java
+++ src/main/java/test/running/weather/web/WeatherController.java
@@ -1,6 +1,12 @@
 package test.running.weather.web;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
+
+import com.google.gson.Gson;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
 import lombok.RequiredArgsConstructor;
-import test.running.weather.domain.WeatherData;
 import test.running.weather.domain.WeatherDataRepository;
 import test.running.weather.service.WeatherPlaceService;
 import test.running.weather.web.dto.dataCraw.PlaceDto;
-import test.running.weather.web.dto.dataCraw.ResponseDto;
+import test.running.weather.web.dto.dataCraw.RootDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -27,14 +31,13 @@ public class WeatherController {
 
     @GetMapping("/")
     public String listPlace(@RequestParam(defaultValue = "0") Integer page, Model model) {
-        List<WeatherData> totalList = weatherDataRepository.findAll();
-        model.addAttribute("total", totalList);
         return "page/main";
     }
 
-    @RequestMapping(value = "/weather/{courseId}", method = RequestMethod.GET, produces = "application/json; charset=UTF8")
-    public @ResponseBody String jsonList(@PathVariable String courseId, Model model) {
-        return weatherPlaceService.rowNumDown(courseId);
+    @RequestMapping(value = "/{courseId}", method = RequestMethod.GET, produces = "application/json; charset=UTF8")
+    public String jsonList(@PathVariable String courseId, Model model) {
+        model.addAttribute("Item", weatherPlaceService.placeList(courseId));
+        return "page/find";
     }
 
 }
